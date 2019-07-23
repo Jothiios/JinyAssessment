436b1ed3ec9accdb6f91966641c240f7bdbbce8a
Nadella Ravindra <ravindra0529@gmail.com>
2019-07-23 15:08:04 +0530

    Initial Commit

---
+++ jinyAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jinyAssessment
+//
+//  Created by Nadella Ravindra on 23/07/19.
+//  Copyright © 2019 Widas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
